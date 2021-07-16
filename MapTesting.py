b26ca3122e462591c1c6624e4e8750edd8bbb54c
chiqui1986 <45525387+chiqui1986@users.noreply.github.com>
2021-07-16 17:46:12 -0300

    Agrego features de capas layout al mapa

--- MapTesting.py
+++ MapTesting.py
@@ -7,6 +7,7 @@ data = pandas.read_csv("Parques.txt")
 lon = list(data["LONG"])
 lat = list(data["LAT"])
 noms = list(data["AREA PROTEGIDA"])
+reg = list(data["REGION"])
 visit = list(data["VISITANTES ANUALES"])
 mapa = folium.Map(location = [-34.96, -65.11], zoom_start=6)
 
@@ -26,10 +27,10 @@ def color_pop_up(a):
         print("sin datos")
         return "gray"
 
-fg = folium.FeatureGroup(name = "Mapa de Parques Nacionales en Argentina")
+fPar = folium.FeatureGroup(name = "Parques Nacionales")
 
 for lt, ln, nom, vis in zip(lat, lon, noms, visit):
-    mapa.add_child(folium.Marker
+    fPar.add_child(folium.Marker
                    (location = [lt, ln],
                    popup = folium.Popup(str(nom) + "** Visitantes anuales: " + str(vis) + " **", parse_html = True, max_width = "80%"),
                    tooltip = str(nom),
@@ -38,10 +39,20 @@ for lt, ln, nom, vis in zip(lat, lon, noms, visit):
                    fill_opacity = 0.7))
 
 
-fg.add_child(folium.GeoJson(data = (open("world.json", "r", encoding = "utf-8-sig").read()),
+fPob = folium.FeatureGroup(name = "Paises por poblacion")
+
+fPob.add_child(folium.GeoJson(data = (open("world.json", "r", encoding = "utf-8-sig").read()),
                             style_function=lambda x: {"fillColor": "green" if x["properties"]["POP2005"] < 10000000
                                                       else "orange" if 10000000<= x["properties"]["POP2005"] < 20000000
                                                       else "red",
                                                       "weight": 0}))
-mapa.add_child(fg)
+
+fReg = folium.FeatureGroup(name="Region")
+mapa.add_child(fPar)
+mapa.add_child(fPob)
+
+mapa.add_child(folium.LayerControl())
+
+
+
 mapa.save("mapaParques.html")
